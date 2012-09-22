7c0f44a554eaf1bad3b75a290c2d4f167a2c0edf
Zach Denton <zacharydenton@gmail.com>
2012-09-21 21:31:10 -0400

    Add Item model

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, redirect, session, url_for, render_template
 from flask_cas import flask_cas
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(flask_cas)
 app.secret_key = 'this/ought%to*be^changed'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String)
+    email = db.Column(db.String(120))
 
 @app.route("/")
 def index():
