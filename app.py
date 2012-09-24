379c3fbc2007e794c318fd71e68d11aafa4950e8
Zach Denton <zacharydenton@gmail.com>
2012-09-24 15:55:17 +0000

    Implement marking item as 'found'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, session, url_for, render_template, send_from_directory
+from flask import Flask, request, redirect, session, url_for, render_template, send_from_directory, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
 from flask_cas import flask_cas, login_required
@@ -13,12 +13,16 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(200))
     email = db.Column(db.String(120))
+    claimed = db.Column(db.Boolean, default=False)
 
     def image_url(self):
         if app.debug:
             return url_for('image', filename=self.image)
         else:
             return '/images/%s' % self.image
+
+    def __repr__(self):
+        return "Item %s" % (self.id)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -49,17 +53,29 @@ if app.debug:
     @app.route("/images/<filename>")
     @login_required
     def image(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+        image_dir = os.path.join(app.config['UPLOAD_FOLDER'], 'images')
+        return send_from_directory(image_dir, filename)
+
+@app.route("/item/<int:item_id>/claim/")
+@login_required
+def claim_item(item_id):
+    item = Item.query.get(item_id)
+    item.claimed = not item.claimed
+    db.session.commit()
+    return redirect(url_for('item', item_id=item_id))
 
 @app.route("/item/<int:item_id>/")
 @login_required
 def item(item_id):
-    return render_template('item.html', item=Item.query.get(item_id))
+    item = Item.query.get(item_id)
+    if item is None:
+        abort(404)
+    return render_template('item.html', item=item, get_email=get_email)
 
 @app.route("/")
 @login_required
 def index():
-    return render_template('index.html', items=Item.query.all())
+    return render_template('index.html', items=Item.query.filter_by(claimed=False))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=app.debug, port=5001)
