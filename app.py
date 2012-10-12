ca6bcd90561fde1c968fda59bb57273fb71a169a
Marco A. Peraza <marcoaperaza@gmail.com>
2012-10-12 18:02:20 -0400

    Add user and claims models and updated actions and templates appropriately

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, session, url_for, render_template, send_from_directory, abort
+from flask import Flask, request, redirect, session, url_for, render_template, send_from_directory, abort, g
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_cas import flask_cas, login_required
 
@@ -8,13 +8,18 @@ app.config.from_pyfile('local_settings.py')
 app.register_blueprint(flask_cas)
 db = SQLAlchemy(app)
 
+claims_table = db.Table('claims',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('item_id', db.Integer, db.ForeignKey('item.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     netid = db.Column(db.String(15), unique=True)
     full_name = db.Column(db.String(200))
 
     found_items = db.relationship("Item", backref="finder")
-    claims = db.relationship("Item", secondary=claims, backref="claimers")
+    claims = db.relationship("Item", secondary=claims_table, backref="claimers")
 
     def __init__(self, full_name, netid):
         self.full_name = full_name
@@ -25,11 +30,6 @@ class User(db.Model):
 
     def __repr__(self):
         return str(self.netid)
-
-claims = db.Table('claims',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('item_id', db.Integer, db.ForeignKey('item.id'))
-)
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,14 +52,14 @@ def allowed_file(filename):
 
 @app.before_request
 def fetch_user():
-    if 'cas_user' in session:
-        g['user'] = User.query.filter_by(netid=session['cas_user']['netid'].first())
-        if g['user'] is None:
-            g['user'] = User(session['cas_user']['name'], session['cas_user']['netid'])
-            db.session.add(g['user'])
+    if 'user' in session:
+        g.user = User.query.filter_by(netid=session['user']['netid']).first()
+        if g.user is None:
+            g.user = User(session['user']['name'], session['user']['netid'])
+            db.session.add(g.user)
             db.session.commit()
 
-@app.route("/add/", methods=["POST"])
+@app.route("/add/", methods=["GET", "POST"])
 @login_required
 def add():
     if request.method == "POST":
@@ -69,7 +69,7 @@ def add():
             if not os.path.isdir(image_dir):
                 os.makedirs(image_dir)
             item = Item()
-            item.finder = g['user']
+            item.finder = g.user
             db.session.add(item)
             db.session.commit()
             filename = "%s.%s" % (item.id, image.filename.rsplit('.')[-1])
@@ -85,19 +85,19 @@ if app.debug:
         image_dir = os.path.join(app.config['UPLOAD_FOLDER'], 'images')
         return send_from_directory(image_dir, filename)
 
-@app.route("/item/<int:item_id>/claim/")
+@app.route("/item/<int:item_id>/claim/", methods=["POST"])
 @login_required
 def claim_item(item_id):
     item = Item.query.get(item_id)
-    item.claimers.append(g["user"])
+    item.claimers.append(g.user)
     db.session.commit()
     return redirect(url_for('item', item_id=item_id))
 
-@app.route("/item/<int:item_id>/unclaim")
+@app.route("/item/<int:item_id>/unclaim", methods=["POST"])
 @login_required
-def claim_item(item_id):
+def unclaim_item(item_id):
     item = Item.query.get(item_id)
-    item.claims.remove(g["user"])
+    item.claimers.remove(g.user)
     db.session.commit()
     return redirect(url_for('item', item_id=item_id))
 
@@ -107,12 +107,12 @@ def item(item_id):
     item = Item.query.get(item_id)
     if item is None:
         abort(404)
-    return render_template('item.html', item=item, cur_user=g['user'])
+    return render_template('item.html', item=item, cur_user=g.user)
 
 @app.route("/")
 @login_required
 def index():
-    return render_template('index.html', items=Item.query.filter_by(claimed=False))
+    return render_template('index.html', items=Item.query.all())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=app.debug, port=5001)
